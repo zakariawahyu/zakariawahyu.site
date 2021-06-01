03444620bdb3d87f6a8cfc1ced35fccf4c71c487
Zakaria Wahyu <zakarianur6@gmail.com>
2021-06-01 15:00:27 +0700

    Edit contact form use PHPMailer

--- form/contact.php
+++ form/contact.php
@@ -3,18 +3,32 @@
  *  CONFIGURE EVERYTHING HERE
  */
 
-// an email address that will be in the From field of the email.
-$from = 'Demo contact form <zakarianur6@gmail.com>';
+// get PHPMailer
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-// an email address that will receive the email with the output of the form
-$sendTo = 'Demo contact form <zakarianur6@gmail.com>';
+require_once "PHPMailer/PHPMailer.php";
+require_once "PHPMailer/Exception.php";
+require_once "PHPMailer/OAuth.php";
+require_once "PHPMailer/POP3.php";
+require_once "PHPMailer/SMTP.php";
 
-// subject of the email
-$subject = 'New message from contact form';
+$mail = new PHPMailer;
 
-// form field names and their translations.
-// array variable name => Text to appear in the email
-$fields = array('InputName' => 'Name', 'email' => 'InputEmail', 'InputMessage' => 'Message'); 
+// setting SMTP Server
+//Set PHPMailer to use SMTP.
+$mail->isSMTP();
+//Set SMTP host name
+$mail->Host = "piyaman.idweb.host";
+//Set this to true if SMTP host requires authentication to send email
+$mail->SMTPAuth = true;
+//Provide username and password
+$mail->Username = "admin@zakariawahyu.com";
+$mail->Password = "@bobokaja123";
+//If SMTP requires TLS encryption then set it
+$mail->SMTPSecure = "ssl";
+//Set TCP port to connect to
+$mail->Port = 465;
 
 // message that will be displayed when everything is OK :)
 $okMessage = 'Your message successfully submitted. Thank you, I will get back to you soon!';
@@ -22,44 +36,19 @@ $okMessage = 'Your message successfully submitted. Thank you, I will get back to
 // If something goes wrong, we will display this message.
 $errorMessage = 'There was an error while submitting the form. Please try again later';
 
-/*
- *  LET'S DO THE SENDING
- */
+$mail->From = "admin@zakariawahyu.com";
+$mail->FromName = $_POST['InputName'];
+$mail->addAddress('zakarianur6@gmail.com', 'Zakaria Wahyu Nur Utomo');
+$mail->isHTML(true);
 
-// if you are not debugging and don't need error reporting, turn this off by error_reporting(0);
-error_reporting(E_ALL & ~E_NOTICE);
+$mail->Subject = $_POST['InputSubject'];
+$mail->Body    = "<i>From : ".$_POST['InputEmail']."</i><br><i>Message : ".$_POST['InputMessage']."</i>";
 
-try
-{
-
-    if(count($_POST) == 0) throw new \Exception('Form is empty');
-            
-    $emailText = "You have a new message from your contact form\n=============================\n";
-
-    foreach ($_POST as $key => $value) {
-        // If the field exists in the $fields array, include it in the email 
-        if (isset($fields[$key])) {
-            $emailText .= "$fields[$key]: $value\n";
-        }
-    }
-
-    // All the neccessary headers for the email.
-    $headers = array('Content-Type: text/plain; charset="UTF-8";',
-        'From: ' . $from,
-        'Reply-To: ' . $from,
-        'Return-Path: ' . $from,
-    );
-    
-    // Send email
-    mail($sendTo, $subject, $emailText, implode("\n", $headers));
-
+if (!$mail->send()) {
+    $responseArray = array('type' => 'danger', 'message' => $errorMessage);
+} else{
     $responseArray = array('type' => 'success', 'message' => $okMessage);
 }
-catch (\Exception $e)
-{
-    $responseArray = array('type' => 'danger', 'message' => $errorMessage);
-}
-
 
 // if requested by AJAX request return JSON response
 if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
